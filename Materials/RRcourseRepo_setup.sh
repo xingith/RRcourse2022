a224c1003dcf58d85f1f512eb4613be037d76e38
WHardyUW <78858034+WHardyUW@users.noreply.github.com>
2022-03-30 09:41:03 +0200

    added github setup file

---
+++ Materials/RRcourseRepo_setup.sh
@@ -0,0 +1,12 @@
+
+# Put your GitHub repository HTTPS link in the place of <url>
+
+git clone <url>
+
+cd RRcourse2022
+
+git remote add courseRepo https://github.com/WHardyUW/RRcourse2022.git
+
+git fetch courseRepo
+
+git merge courseRepo/main
